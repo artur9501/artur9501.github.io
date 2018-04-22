588fc7b1ab4f85a7808d4a83b6a31d500f2af642
Artur Młodziński <artur.mlody11@wp.pl>
2018-04-22 16:53:57 +0200

    Zadanie 22.04

---
+++ warsztaty/funkcja iloczynu js/js/funkcja.js
@@ -0,0 +1,6 @@
+function iloczyn(element1, element2, element3) {
+    var result = element1 * element2 * element3;
+    return result;
+}
+var wynikFunkcji = iloczyn(2, 3, 4);
+console.log(wynikFunkcji);
