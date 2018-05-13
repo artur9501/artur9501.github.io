f408d399a6c95edf774cbae4cf434c05f2cbcc79
Artur Młodziński <artur.mlody11@wp.pl>
2018-05-13 16:53:24 +0200

    add blue top nav

--- warsztaty/Projekt-bootstrap/js/main.js
+++ warsztaty/Projekt-bootstrap/js/main.js
@@ -1,1 +1,16 @@
-
+$(function () {
+    $("a[href*='#']:not([href='#'])").click(function (e) {
+        $("html, body").animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
+    //scroll nav color chage
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            console.log("to jest", $("#main-nav"))
+            $("#main-nav").addClass("scroll");
+        } else {
+            $("#main-nav").removeClass("scroll");
+        }
+    });
+});
